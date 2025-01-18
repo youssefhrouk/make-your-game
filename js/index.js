ff98f81134066a51972b057cfa203c0b21d2706a
youssefhrouk <yhrouk@gmail.com>
2025-01-18 18:46:57 +0100

    new modifications

--- js/index.js
+++ js/index.js
@@ -1,56 +1,48 @@
 const boxEnemy = document.getElementById("boxEnemy");
 const boxShooter =document.getElementById("boxShooter");
+const gameContainer = document.getElementById("game-container");
 
+let enemyX = 0,enemyY = 0,enemyBulletFire;
+let enemySpeed = 2;
 let animationId;
 let position=0;
 let score =0;
-/*je crée une fonction pour déplacer l'ennemi*/
 
-// function ADDElement(elem, classs, content) {
-//   let posts = document.getElementById("posts")
-
-//   let x = document.createElement(elem)
-//   x.classList.add(classs)
-
-//   if (content != "") {
-//       x.innerText = content
-//   }
-
-//   return x
-// }
-
-function createDiv(){
- const div = document.createElement('div')
- div.style.width = "100px";
- div.style.height = "50px";
- div.style.position = "absolute";
- div.style.top = `${position}px`;
- div.style.left = `${position}px`;
- div.style.background = "red";
- div.style.borderRadius = "50%";
-
- return div
-}
-function animate(timestamp){
-  position += 1;
-  boxEnemy.style.left= position+"px";
-  if (position<1000){
-  animationId=requestAnimationFrame(animate); 
-
-  }else{
-    position=0;
-    boxEnemy.style.left= position+"px";
-   animationId= requestAnimationFrame(animate)
+export function createEnemies(enemyCount){
+  for (let i=0;i<enemyCount;i++){
+    const enemy = document.createElement('div');
+    enemy.classList.add("enemy");
+    gameContainer.appendChild(enemy);
   
   }
+  
+}
+
+function animate(timestamp){
+  enemyX += enemySpeed;
+  boxEnemy.style.left= `${enemyX}px`;
+  if (enemyX + boxEnemy.offsetWidth > gameContainer.offsetWidth || enemyX < 0) {
+    enemySpeed *= -1; // Reverse direction
+    enemyY += 20; // Move the enemy down
+    boxEnemy.style.top = `${enemyY}px`;
+  }
+
+  // Reset enemy position if it goes below the container
+  if (enemyY > gameContainer.offsetHeight) {
+    enemyX = 0;
+    enemyY = 0;
+    boxEnemy.style.left = `${enemyX}px`;
+    boxEnemy.style.top = `${enemyY}px`;
+  }
+
+  animationId = requestAnimationFrame(animate); // Continue the animation
 }
 requestAnimationFrame(animate);
 
 
 
 boxShooter.addEventListener("click",()=>{
-  console.log("hh");
   
-const div = createDiv();
-document.body.appendChild(div);
+// const div = createDiv();
+// document.body.appendChild(div);
 })
