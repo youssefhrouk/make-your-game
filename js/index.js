91e6794a15179c766a83f4671807ec1d5d3ad900
yhrouk <yhrouk@gmail.com>
2025-01-22 18:19:05 +0100

    finally I SHow the ship

--- js/index.js
+++ js/index.js
@@ -1,71 +1,62 @@
 import { createShip, moveShip } from "./ship.js";
 
 export const gameDiv = document.querySelector(".game");
-export let boxBCR = document.querySelector(".box").getBoundingClientRect;
-
+export let boxBCR = document.querySelector(".box").getBoundingClientRect();
+const titleDiv = document.querySelector(".title");
 export let gameRunning = false;
-// export let gameOver = false;
-// export let gamePaused = false;
+export let gameOver = false;
+export let gamePaused = false;
 export const gameKeys = {
-  ArrowLeft : false,
-  ArrowRight : false,
-  Space : false,
+  ArrowLeft: false, 
+  ArrowRight: false,
+  Space: false,
 }
 
-
-
-window.onload = function() {
+window.addEventListener("load", () => {
+  createShip();
   const startGameElement = document.getElementById('startGame');
 
   setInterval(() => {
-      startGameElement.classList.toggle('hidden');
+    startGameElement.classList.toggle('hidden');
   }, 700);
-};
-
-window.addEventListener("load",()=>{
   
-  createShip();
+});
 
-})
 
-document.addEventListener("keydown",(e)=>{
-  if (e.code in gameKeys){
-    gameKeys[e.code] = true;
-  }
-  if(e.code=="Enter"){
-    if (gameRunning){
 
-      console.log("par iciiii");
-    }
+
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft") gameKeys['ArrowLeft'] = true;
+  if (e.code === "ArrowRight") gameKeys['ArrowRight'] = true;
+  
+  if (e.code == "Space" || e.key === " " ) {
+      if (gameRunning){
+
+        gameKeys["Space"] = true;
+      }
+      if (!gameRunning){
+        titleDiv.remove();
+        gameDiv.removeAttribute('hidden');
+        gameRunning = true;
+      }
+
     
-    startGame();
   }
 
 });
 
-document.addEventListener("keyup",(e)=>{
-  if (e.code in gameKeys){
+document.addEventListener("keyup", (e) => {
+  if (e.code in gameKeys) {
     gameKeys[e.code] = false;
   }
-
-  
 })
 
 
-
-function startGame(){
-  if (!gameRunning){
-    gameRunning = true;  
-
-    console.log("Game started! 1111111111",gameRunning);
-    
-    
-  }
+function startGame() {
+  moveShip();
   requestAnimationFrame(startGame);
 
-  
-  moveShip();  
 }
 
 startGame();
-  // console.log("datdgh 086666666");
--- js/ship.js
+++ js/ship.js
@@ -8,21 +8,20 @@ let shipX,shipY;
 export function createShip(){
     shipX = boxBCR.width/2-25;
     shipY = boxBCR.height-75;
-    ship.src = "../images/alien.png";
+    ship.src = "../images/ship.png";
     ship.setAttribute("class","ship");
     ship.width = 50;
     ship.style.transform = `translate(${shipX}px,${shipY}px)`;
-    gameDiv.appendChild(ship)
-    console.log("zzzz");
+     gameDiv.appendChild(ship)
 }
 
 export function moveShip(){
+    console.log("Moving ship..."); // Debug
     if (gameRunning){
-        
         if(gameKeys.ArrowLeft && shipX >= 2) shipX -=5;
-        if(gameKeys.ArrowRight && shipX < boxBCR.width - 52) shipX += 5;
-        
+        if(gameKeys.ArrowRight && shipX < boxBCR.width - 52) shipX += 5;        
     }
+    
     ship.style.transform = `translate(${shipX}px,${shipY}px)`;
     
 }
